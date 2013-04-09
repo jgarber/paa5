6ab1fdb376c1bf252c3c95c86b53c8d17ceea9ab
Jason Garber <jg@jasongarber.com>
2013-04-09 14:56:11 -0400

    Normalfy paa5::default recipe

--- cookbooks-src/paa5/recipes/default.rb
+++ cookbooks-src/paa5/recipes/default.rb
@@ -6,11 +6,6 @@
 #
 # See LICENSE for details
 #
-class Chef::Recipe
-    # mix in recipe helpers
-    include Chef::RubyBuild::RecipeHelpers
-end
-
 include_recipe "rvm::gem_package"
 
 node['rvm']['default_ruby'] = node['paa5']['ruby_version']
@@ -18,13 +13,12 @@ node['rvm']['gem_package']['rvm_string'] = node['paa5']['ruby_version']
 node['rvm']['group_users'] = ['vagrant']
 
 include_recipe "apt"
-package "build-essential"
+include_recipe "build-essential"
+include_recipe "rvm::vagrant"
+include_recipe "rvm::system"
 include_recipe "nodejs"
 include_recipe "database::postgresql"
 include_recipe "postgresql::server"
-
-include_recipe "rvm::vagrant"
-include_recipe "rvm::system"
 
 rvm_gem "bundler"
 rvm_gem "bluepill"
