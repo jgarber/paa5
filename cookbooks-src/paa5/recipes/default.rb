e1e9ecae5f3f30373f35dcfb1454cb03975d33bb
Jason Garber <jg@jasongarber.com>
2013-04-09 15:16:51 -0400

    Convert node assignment to Chef 11 style

--- cookbooks-src/paa5/recipes/default.rb
+++ cookbooks-src/paa5/recipes/default.rb
@@ -6,11 +6,11 @@
 #
 # See LICENSE for details
 #
-include_recipe "rvm::gem_package"
+node.normal['rvm']['default_ruby'] = node['paa5']['ruby_version']
+node.normal['rvm']['gem_package']['rvm_string'] = node['paa5']['ruby_version']
+node.normal['rvm']['group_users'] = ['vagrant']
 
-node['rvm']['default_ruby'] = node['paa5']['ruby_version']
-node['rvm']['gem_package']['rvm_string'] = node['paa5']['ruby_version']
-node['rvm']['group_users'] = ['vagrant']
+include_recipe "rvm::gem_package"
 
 include_recipe "apt"
 include_recipe "build-essential"
@@ -23,8 +23,8 @@ include_recipe "postgresql::server"
 rvm_gem "bundler"
 rvm_gem "bluepill"
 rvm_gem "foreman"
-node["bluepill"]["bin"] = "#{node[:languages][:ruby][:gems_dir]}/bin/bluepill"
-node['nginx']['init_style'] = "bluepill"
+node.normal["bluepill"]["bin"] = "#{node[:languages][:ruby][:gems_dir]}/bin/bluepill"
+node.normal['nginx']['init_style'] = "bluepill"
 include_recipe "nginx::source"
 
 include_recipe "paa5::git_setup"
